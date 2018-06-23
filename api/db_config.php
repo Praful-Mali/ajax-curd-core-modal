759b7aebffeb241e3761351a001d99ebffbcae2c
praful <ankitmali@yandex.com>
2018-06-24 01:32:29 +0530

    first commit

---
+++ api/db_config.php
@@ -0,0 +1,8 @@
+<?php
+	define("LOCALHOST", "localhost");
+	define("USERNAME", "root");
+	define("PASSWORD", "");
+	define("DATABASE", "vadodara");
+
+	$mysqli = new mysqli(LOCALHOST, USERNAME, PASSWORD, DATABASE);
+?>
